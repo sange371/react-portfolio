6971c492631eda452627ad9e326ac20e57ff343f
sange371 <zusangembaleki@icloud.com>
2026-01-08 20:48:09 +0200

    updated container bg-color, updated paragraphs to be more clear

--- react-portfolio/src/Homefile.js
+++ react-portfolio/src/Homefile.js
@@ -3,25 +3,28 @@ import Navbar from './Navbar';
 import githubLogo from './icons8-github-logo-64.png';
 import linkedinLogo from './icons8-linkedin-logo-50.png';
 import instagramLogo from './icons8-instagram-logo-64.png';
-import myImage from './my-image.png';
+import myImage from './portfolio-image.jpg';
 
 import './Homefile.css'
 
 export default function Homefile(){
     return(
        
-        <div className="portfolio-container bg-dark">
+        <div className="portfolio-container ">
             <Navbar/>
             <div className="grid-container">
                 <div className="info">
                     <h1>Hi, I'm Zusange Mbaleki</h1>
-                    <h4>a Front-end developer learner at SheCodes</h4>
+                    <h4><strong>a Front-end developer learner at SheCodes</strong></h4>
                     <br/>
-                    <p>I’m a front-end developer with recent training from the SheCodes workshop.</p><p> I love creating clean, responsive websites that deliver a smooth user experience.
-                    </p>
+                    <p><strong>I’m a front-end developer with recent training from the SheCodes workshop.</strong></p>
+                    <p><strong>I love creating clean, responsive websites that deliver a smooth user experience.</strong></p>
+                    <button className="contact-btn"><a href="mailto:zusangembaleki@icloud.com" className="contact-me">Contact Me</a></button>
+                    
+                    <button className="work-btn"><a href="/" className="my-work">See My Work</a></button>
                 </div>
                 <div className="self-image"> 
-                    <img src={myImage} className="my-image img-fluid"alt="coding woman"/>
+                    <img src={myImage} className="my-image img-fluid" alt="coding woman"/>
                    
                     
                 </div>
